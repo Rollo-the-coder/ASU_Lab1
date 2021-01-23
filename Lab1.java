76eda840e82c19b032f7e66cb70e2d08cac3fb04
Rollo-the-coder <erikgotta@gmail.com>
2021-01-23 23:40:45 +0700

    ez pz lab1

---
+++ Lab1.java
@@ -0,0 +1,35 @@
+//=================================================
+//  Lab1.java
+//  Name: Erik Christian Gotta
+//  ASU ID:
+//  Time taken to complete this lab: 15 minutes
+//=================================================
+
+public class Lab1
+{
+    public static void main(String[] args) 
+    {
+        //integers
+        int intnum_1 = 13;
+        int intnum_2 = 18;
+
+        //doubles
+        double realnum_1 = 7.0;
+        double realnum_2 = 33.0;
+
+        //chars
+        char char_1 = 'z';
+        char char_2 = 'A';
+
+        //booleans
+        boolean bool_1 = true;
+        boolean bool_2 = false;
+
+        //display
+        System.out.println("Integer Value 1 = " + intnum_1 + " and Integer Value 2 = " + intnum_2);
+        System.out.println("Double Value 1 = " + realnum_1 + " and Double Value 2 = " + realnum_2);
+        System.out.println("Character Value 1 = " + char_1 + " and Character Value 2 = " + char_2);
+        System.out.println("Boolean Value 1 = " + bool_1 + " and Boolean Value 2 = " + bool_2);
+
+    }
+}
