40ac54933acc98845227d31face40584079c8088
Rollo-the-coder <erikgotta@gmail.com>
2021-02-07 10:29:16 +0700

    Updated if style to my JS style of writing

--- Lab2.java
+++ Lab2.java
@@ -7,6 +7,8 @@
 
 import java.util.Scanner;
 
+
+//Class to use scanner and if statements 
 class Lab2
 {
     public static void main(String args[])
@@ -16,23 +18,14 @@ class Lab2
         double input1 = 500;
         double input2 = 600;
 
+        //Input collection
         System.out.print("Please enter the first number: ");
         input1 = sc.nextDouble();
-
 
         System.out.print("Please enter the second number: ");
         input2 = sc.nextDouble();
 
-        // if (input1 >= input2)
-        // {
-        //     System.out.println("The sum is " + (input1 + input2));
-        // }
-        // else
-        // {
-        //     System.out.println("The difference is " + (input2 - input1));
-        // }
-
-
+        //If statement to look at which number is larger and then either find the sum or the difference
         if (input1 >= input2) {
             System.out.println("The sum is " + (input1 + input2));
         } else {
